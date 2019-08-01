efac2586b0a04b5bd1f748a103fc045d01a7d485
grumpydev-net <adrian@adrianpadilla.net>
2019-08-01 02:16:43 -0700

    Footer

---
+++ code/mvc-website/Views/Shared/Components/Footer/FooterViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_website.Views.Components.FooterComponent
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public FooterViewComponent()
+        {
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View());
+        }
+    }
+}
