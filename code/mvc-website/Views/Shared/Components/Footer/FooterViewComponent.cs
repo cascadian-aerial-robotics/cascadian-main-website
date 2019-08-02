5d1d24ea2a68b10ee07b274c4af9a0e267fd855e
Adrian Padilla <adpadi@microsoft.com>
2019-08-01 21:08:50 -0700

    Added Jumbotron and menus

--- code/mvc-website/Views/Shared/Components/Footer/FooterViewComponent.cs
+++ code/mvc-website/Views/Shared/Components/Footer/FooterViewComponent.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
-namespace mvc_website.Views.Components.FooterComponent
+namespace CascadianAerialRobotics.Website.Views.Components.FooterComponent
 {
     public class FooterViewComponent : ViewComponent
     {
---
+++ code/mvc-website/Views/Shared/Components/Header/HeaderViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CascadianAerialRobotics.Website.Views.Shared.Components.Header
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View());
+        }
+    }
+}
