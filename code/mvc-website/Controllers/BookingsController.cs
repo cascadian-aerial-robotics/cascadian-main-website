5e3952b080e9ed76c002bed8bd067d3abd6e418e
grumpydev-net <adrian@adrianpadilla.net>
2019-08-09 02:11:34 -0700

    Added a much needed 404 page and a redirect

---
+++ code/mvc-website/Controllers/BookingsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_website.Controllers
+{
+    public class BookingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            // TODO: Do not hardcode this
+            return this.Redirect("https://outlook.office365.com/owa/calendar/CascadianAerialRobotics@cascadianaerialrobotics.com/bookings/");
+        }
+    }
+}
