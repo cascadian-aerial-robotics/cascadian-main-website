86e1b2ef87aee79a39f58e8d84bfb1bd606c777b
grumpydev-net <adrian@adrianpadilla.net>
2019-09-08 02:29:24 -0700

    Added model to handle page metadata

--- code/mvc-website/Controllers/HomeController.cs
+++ code/mvc-website/Controllers/HomeController.cs
@@ -21,37 +21,61 @@ namespace CascadianAerialRobotics.Website.Controllers
 
         public IActionResult Index()
         {
-            return View(new CommonComponentModel { PubliclyExposedStringsProvider = this.PubliclyExposedStringsProvider });
+            var pageinfo = new PageMetadataModel
+            {
+                Page = "Cascadian Aerial Robotics LLC - Drone mapping, photogrammetry and GIS services in the Pacific Northwest",
+                Description = "Cascadian Aerial Robotics LLC offers drone and mapping services to Washington State and the Pacific Northwest in general."
+            };
+
+            return View(new CommonComponentModel { PubliclyExposedStringsProvider = this.PubliclyExposedStringsProvider, Metadata  = pageinfo });
         }
 
         [Route("Privacy")]
         public IActionResult Privacy()
         {
-            return View("Privacy", new CommonComponentModel { PubliclyExposedStringsProvider = this.PubliclyExposedStringsProvider });
+            var pageinfo = new PageMetadataModel
+            {
+                Page = "Privacy | Cascadian Aerial Robotics LLC",
+                Description = "Privacy policy that regulates the website for Cascadian Aerial Robotics LLC."
+            };
+
+            return View("Privacy", new CommonComponentModel { PubliclyExposedStringsProvider = this.PubliclyExposedStringsProvider, Metadata = pageinfo });
         }
 
         [Route("Newsletter")]
         public IActionResult Newsletter()
         {
-            return View("Newsletter", new CommonComponentModel { PubliclyExposedStringsProvider = this.PubliclyExposedStringsProvider });
-        }
+            var pageinfo = new PageMetadataModel
+            {
+                Page = "Feathered flight: The Cascadian Aerial Robotics newsletter",
+                Description = "The current issue of Feathered flight, the Cascadian Aerial Robotics newsletter."
+            };
 
-        [Route("Newsletter2")]
-        public IActionResult Newsletter2()
-        {
-            return View("Newsletter2", new CommonComponentModel { PubliclyExposedStringsProvider = this.PubliclyExposedStringsProvider });
+            return View("Newsletter", new CommonComponentModel { PubliclyExposedStringsProvider = this.PubliclyExposedStringsProvider, Metadata = pageinfo });
         }
 
         [Route("Bookings")]
         public IActionResult Bookings()
         {
-            return View("Bookings", new CommonComponentModel { PubliclyExposedStringsProvider = this.PubliclyExposedStringsProvider });
+            var pageinfo = new PageMetadataModel
+            {
+                Page = "Bookings | Cascadian Aerial Robotics LLC",
+                Description = "Book a meeting or call with Cascadian Aerial Robotics."
+            };
+
+            return View("Bookings", new CommonComponentModel { PubliclyExposedStringsProvider = this.PubliclyExposedStringsProvider, Metadata = pageinfo });
         }
 
         [Route("FAQ")]
         public IActionResult Faq()
         {
-            return View("Faq", new CommonComponentModel { PubliclyExposedStringsProvider = this.PubliclyExposedStringsProvider });
+            var pageinfo = new PageMetadataModel
+            {
+                Page = "Frequently asked questions | Cascadian Aerial Robotics LLC",
+                Description = "The questions some of our customers or potential customers often have."
+            };
+
+            return View("Faq", new CommonComponentModel { PubliclyExposedStringsProvider = this.PubliclyExposedStringsProvider, Metadata = pageinfo });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- code/mvc-website/Models/CommonComponentModel.cs
+++ code/mvc-website/Models/CommonComponentModel.cs
@@ -5,6 +5,7 @@ namespace CascadianAerialRobotics.Website.Models
 {
     public class CommonComponentModel
     {
+        public PageMetadataModel Metadata { get; set; }
         public IPubliclyExposedStringsProvider PubliclyExposedStringsProvider { get; set; }
     }
 }
